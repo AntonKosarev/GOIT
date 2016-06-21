a9da9b45fe2461f5e9797c0c4f9ae65ba5d8be25
Kosarev Anton <rasto4itel@gmail.com>
2016-06-21 13:14:14 +0300

    jast commit

--- DZJS_15-16/js/script.js
+++ DZJS_15-16/js/script.js
@@ -1,27 +1,30 @@
+
 $(function(){
 
-    window.pixabayCallback = function (data) {
-        var ul = document.createElement("ul");
-        $.each(data.results, function(i, val){
-            var li = document.createElement("li");
-            li.innerHTML = '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+"</a> - "+val.content;
-            ul.appendChild(li);
+    //var text = 'yellow flowers';
+    var $button = $('#button');
+    var $input;
+    function getSearch(e) {
+        e.preventDefault();
+        console.log($('.searchcontent'));
+        $('.searchList').html('');
+        $input = $("#search")[0].value;
+        $.ajax({
+            url: 'https://pixabay.com/api/?key=2764229-c5184a27330ccf1853daf3552&q='
+                 + $input + '&image_type=photo',
+            dataType: 'jsonp',
+            lang: 'ru'
+        }).done(function (data) {
+            var html = $('#test').html();
+            var resultSearch = data.hits;
+            var content = tmpl(html, {
+                data: resultSearch
+            });
+            $('.searchcontent').append(content);
+            console.log(resultSearch)
         });
-        $('.wrapper').html(ul);
-    };
-
-    $.ajax({
-        url: 'http://pixabay.com/api',
-        metod: 'GET',
-        dataType: 'jsonp',
-        data: {
-            key: '2764229-c5184a27330ccf1853daf3552',
-            q: 'foto',//$('.search').val(),
-            callback: pixabayCallback
-        }
-    });
+    }
+    $button.on('click', getSearch);
 
 
 });
-
-

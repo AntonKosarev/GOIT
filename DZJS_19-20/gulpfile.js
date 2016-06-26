0dd804b3e0b91df8a49b31c05d9124f216f0d87f
Kosarev Anton <rasto4itel@gmail.com>
2016-06-26 16:05:13 +0300

    jast commit

--- DZJS_19-20/gulpfile.js
+++ DZJS_19-20/gulpfile.js
@@ -1,11 +1,47 @@
+'use strict';
 var gulp = require('gulp');
 
-gulp.task('default', ['concat']);
+gulp.task('default', [
+	'sass',
+	'sass:watch',
+	'concatcss',
+	'concatcss:watch',
+	'cssmin',
+	'cssmin:watch'
+]);
 
-var concat = require('gulp-concat');
-
-gulp.task('concat', function() {
-	return gulp.src('./css/*.css')
-			.pipe(concat('syle.main.css'))
-			.pipe(gulp.dest('./css/'));
+//https://www.npmjs.com/package/gulp-sass
+var sass = require('gulp-sass');
+gulp.task('sass', function () {
+	return gulp.src('css/*.scss')
+			.pipe(sass().on('error', sass.logError))
+			.pipe(gulp.dest('css'));
 });
+gulp.task('sass:watch', function () {
+	gulp.watch('./css/*.scss', ['sass']);
+});
+
+//https://www.npmjs.com/package/gulp-concat-css
+var concatCss = require('gulp-concat-css');
+gulp.task('concatcss', function () {
+	return gulp.src('css/*.css')
+			.pipe(concatCss("style.main.css"))
+			.pipe(gulp.dest('css/dist/'));
+});
+gulp.task('concatcss:watch', function () {
+	gulp.watch('./css/*.css', ['concatcss']);
+});
+
+//https://www.npmjs.com/package/gulp-cssmin
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+gulp.task('cssmin', function () {
+	gulp.src('css/dist/style.main.css')
+			.pipe(cssmin())
+			.pipe(rename({suffix: '.min'}))
+			.pipe(gulp.dest('css/dist'));
+});
+gulp.task('cssmin:watch', function () {
+	gulp.watch('css/dist/*.css', ['cssmin']);
+});
+

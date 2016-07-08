601b303687c01c7e8c93350d600e96bdee55e330
Kosarev Anton <rasto4itel@gmail.com>
2016-07-08 16:19:11 +0300

    jast commit

--- FinalEx/site/js/script.js
+++ FinalEx/site/js/script.js
@@ -9,43 +9,69 @@ $(document).ready(function(e) {
             "top":'0', "left": '0'}).hide().eq(0).show();
     var slideNum = 0;
     var slideTime;
-    slideCount = $(".slider .slide").size();
+    slideCount = [
+        $("#slider .slide").size(),
+        $("#slider_1 .slide").size(),
+        $("#slider_2 .slide").size()
+        ];
+
     var animSlide = function(arrow){
         clearTimeout(slideTime);
         $('.slide').eq(slideNum).fadeOut(hwSlideSpeed);
-        if(arrow == "next"){
-            if(slideNum == (slideCount-1)){slideNum=0;}
-            else{slideNum++}
+        for (var i=0; i<slideCount.length; i++) {
+            if (arrow == "next") {
+                if (slideNum == (slideCount[i] - 1)) {
+                    slideNum = 0;
+                } else {
+                    slideNum++
+                }
+            } else if (arrow == "prew") {
+                if (slideNum == 0) {
+                    slideNum = slideCount[i] - 1;
+                } else {
+                    slideNum -= 1
+                }
+            } else {
+                slideNum = arrow
+            }
         }
-        else if(arrow == "prew")
-        {
-            if(slideNum == 0){slideNum=slideCount-1;}
-            else{slideNum-=1}
-        }
-        else{
-            slideNum = arrow;
-        }
+
         $('.slide').eq(slideNum).fadeIn(hwSlideSpeed, rotator);
         $(".control-slide.active").removeClass("active");
         $('.control-slide').eq(slideNum).addClass('active');
     };
     if(hwNeedLinks){
-        var $linkArrow = $('<a class="prewbutton"></a><a class="nextbutton"></a>')
-            .prependTo('.slider');
-        $('.nextbutton').click(function(){
-            animSlide("next");
-        });
-        $('.prewbutton').click(function(){
-            animSlide("prew");
-        });
+        var $linkArrow = [
+            $('<a id="prewbutton"></a><a id="nextbutton"></a>').prependTo('#slider'),
+            $('<a id="prewbutton_1"></a><a id="nextbutton_1"></a>').prependTo('#slider_1'),
+            $('<a id="prewbutton_2"></a><a id="nextbutton_2"></a>').prependTo('#slider_2')
+            ];
+        $('#nextbutton').click(function(){animSlide("next");});
+        $('#nextbutton_1').click(function(){animSlide("next");});
+        $('#nextbutton_2').click(function(){animSlide("next");});
+        $('#prewbutton').click(function(){animSlide("prew");});
+        $('#prewbutton_1').click(function(){animSlide("prew");});
+        $('#prewbutton_2').click(function(){animSlide("prew");});
     }
     var pause = false;
     var rotator = function(){
         if(!pause){slideTime = setTimeout(function(){animSlide('next')}, hwTimeOut);}
     };
-    $('.slider-wrap').hover(
-        function(){clearTimeout(slideTime); pause = true;},
-        function(){pause = false; rotator();
-        });
+    $sliderWrap = [
+        $('#slider-wrap'),
+        $('#slider-wrap_1'),
+        $('#slider-wrap_2')
+    ];
+    for (var k=0; k<slideCount.length; k++) {
+        $sliderWrap[k].hover(
+            function () {
+                clearTimeout(slideTime);
+                pause = true;
+            },
+            function () {
+                pause = false;
+                rotator();
+            });
+    }
     rotator();
 });

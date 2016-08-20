ce05558a6e1c792eb8273d29967b5dfc663951c1
Kosarev Anton <rasto4itel@gmail.com>
2016-08-21 01:33:35 +0300

    jast commit

--- test_2/src/js/main.js
+++ test_2/src/js/main.js
@@ -1,4 +1,20 @@
 //= partials/jquery.bxslider.js
+
 $(document).ready(function(){
     $('.bxslider').bxSlider();
+});
+
+$(document).ready(function(){
+    function getbg () {
+        $('[data-slide-index = 0]').on('click', function () {
+            $('body').css('background', '#6358c0');
+        });
+        $('[data-slide-index = 1]').on('click', function () {
+            $('body').css('background', '#61acec');
+        });
+        $('[data-slide-index = 2]').on('click', function () {
+            $('body').css('background', '#68a780');
+        });
+    }
+    getbg();
 });

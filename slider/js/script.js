24c1759e3b73d053ced3f4d1ec814ae6ff7ed4ad
Kosarev Anton <rasto4itel@gmail.com>
2016-08-20 09:03:17 +0300

    jast commit

--- slider/js/script.js
+++ slider/js/script.js
@@ -1,74 +1,28 @@
-/* Изменение размера шрифта пропорционально разеру лейаута
-$(window).resize(function(){
-    var text = $(".content_article h1"),
-        currentWidth = parseInt(text.css("width")),
-        newWidth = 85;
-
-    if(currentWidth==1920) newWidth = 85;
-    else if (currentWidth<1920 && currentWidth>=1820) newWidth = 80;
-    else if (currentWidth<1820 && currentWidth>=1720) newWidth = 75;
-    else if (currentWidth<1720 && currentWidth>=1620) newWidth = 70;
-    else if (currentWidth<1620 && currentWidth>=1520) newWidth = 65;
-    else if (currentWidth<1520 && currentWidth>=1420) newWidth = 60;
-    else if (currentWidth<1320 && currentWidth>=1320) newWidth = 55;
-    else if (currentWidth<1220 && currentWidth>=1220) newWidth = 50;
-    else if (currentWidth<1120 && currentWidth>=1120) newWidth = 45;
-    else if (currentWidth<1420 && currentWidth>=1020) newWidth = 40;
-    else if (currentWidth<1420) newWidth = 35;
-
-    text.css("font-size",newWidth+"px");
-//}); */
-//$(document).ready(function(){
-//    var $slide1 = $('.slide1'),
-//        $slide2 = $('.slide2'),
-//        $slide3 = $('.slide3');
-//
-//    var $control1 = $('.controls_1'),
-//        $control2 = $('.controls_2'),
-//        $control3 = $('.controls_3');
-//
-//    $control2.on('click', function(e) {
-//        e.preventDefault();
-//        $slide1.css('display','none');
-//        $slide2.css('display','block');
-//    })
-//});
-
-$(function() {
+$(function(){
     $('.bxslider').bxSlider({
         mode: 'vertical',
         slideMargin: 3
     });
+});
 
-    //var s1 = $('.slide1');
-    //var s2 = $('.slide2');
-    //var s3 = $('.slide3');
+$(function(){
 
-    var $slide1 = $('[data-slide-index = "0"]'),
-        $slide2 = $('[data-slide-index = "1"]'),
-        $slide3 = $('[data-slide-index = "2"]');
+    var $slide1 = $('[data-slide-index = 0]'),
+        $slide2 = $('[data-slide-index = 1]'),
+        $slide3 = $('[data-slide-index = 2]');
 
     var $bg = $('.wrapper');
 
     $slide1.on('click', function(){
         $bg.css('background', '#6358c0');
-        //$s3.css('display', 'none');
-        //$s2.css('display', 'none');
-        //$s1.css('display', 'block');
     });
 
     $slide2.on('click', function(){
         $bg.css('background', '#61acec');
-        //$s3.css('display', 'none');
-        //$s1.css('display', 'none');
-        //$s2.css('display', 'block');
     });
 
     $slide3.on('click', function(){
         $bg.css('background', '#68a780');
-        //$s2.css('display', 'none');
-        //$s1.css('display', '');
-        //$s3.css('display', 'block');
     });
 });
 

c0cf50b30ff0efd1bdbe4498ece93bc68daf1db5
Kosarev Anton <rasto4itel@gmail.com>
2016-08-20 19:15:30 +0300

    project assembly

---
+++ test_1/basegulpfile.js
@@ -0,0 +1,105 @@
+//gulp-autoprefixer
+var autoprefixer = require('gulp-autoprefixer');
+gulp.task('autoprefixer', function () {
+    gulp.src('src/app.css').pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'))
+});
+//gulp-imagemin
+var gulpimagemin = require('gulp-imagemin');
+gulp.task('gulp-imagemin', function () {
+    gulp.src('src/images/*')
+        .pipe(gulpimagemin())
+        .pipe(gulp.dest('dist/images'))
+});
+//imagemin-pngquant
+const imagemin = require('imagemin');
+const imageminPngquant = require('imagemin-pngquant');
+//gulp-uglify
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+
+gulp.task('uglify', function (cb) {
+    pump([
+            gulp.src('lib/*.js'),
+            uglify(),
+            gulp.dest('dist')
+        ],
+        cb
+    );
+});
+//gulp-sass
+var sass = require('gulp-sass');
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+//gulp-rigger
+var rigger = require('gulp-rigger');
+gulp.task('rigger', function () {
+    gulp.src('app/*.js')
+        .pipe(rigger())
+        .pipe(gulp.dest('build/'));
+});
+
+'use strict';
+var gulp = require('gulp');
+////gulp-autoprefixer
+//var autoprefixer = require('gulp-autoprefixer');
+////gulp-imagemin
+//var gulpimagemin = require('gulp-imagemin');
+////imagemin-pngquant
+//var imagemin = require('imagemin');
+//var imageminPngquant = require('imagemin-pngquant');
+////gulp-uglify
+//var uglify = require('gulp-uglify');
+////var pump = require('pump');
+////gulp-sass
+//var sass = require('gulp-sass');
+//gulp-rigger
+var rigger = require('gulp-rigger');
+
+//var path = {
+//    build: { //Тут мы укажем куда складывать готовые после сборки файлы
+//        html: 'build/',
+//        js: 'build/js/',
+//        css: 'build/css/',
+//        img: 'build/img/',
+//        fonts: 'build/fonts/'
+//    },
+//    src: { //Пути откуда брать исходники
+//        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+//        js: 'src/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
+//        style: 'src/style/main.scss',
+//        img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+//        fonts: 'src/fonts/**/*.*'
+//    },
+//    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+//        html: 'src/**/*.html',
+//        js: 'src/js/**/*.js',
+//        style: 'src/style/**/*.scss',
+//        img: 'src/img/**/*.*',
+//        fonts: 'src/fonts/**/*.*'
+//    },
+//    clean: './build'
+//};
+
+//Таск для сборки html:
+
+//gulp.task('html:build', function () {
+//    gulp.src(path.src.html) //Выберем файлы по нужному пути
+//        .pipe(rigger()) //Прогоним через rigger
+//        .pipe(gulp.dest(path.build.html)); //Выплюнем их в папку build
+//});
+
+gulp.task('html:build', function () {
+    gulp.src('src/*.html')
+        .pipe(rigger())
+        .pipe(gulp.dest('build/'));
+});
---
+++ test_1/gulpfile.js
@@ -0,0 +1,36 @@
+'use strict';
+var gulp = require('gulp');
+var rigger = require('gulp-rigger');
+
+var path = {
+    build: { //Тут мы укажем куда складывать готовые после сборки файлы
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: { //Пути откуда брать исходники
+        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: 'src/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
+        style: 'src/style/main.scss',
+        img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        style: 'src/style/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './build'
+};
+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html)
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.html))
+});
+//Собираем javascript
+

778f5caa6ae3fe59d3960bd1ee5ea957312202b9
Kosarev Anton <rasto4itel@gmail.com>
2016-08-22 18:19:19 +0300

    jast commit

--- slider/src/js/main.js
+++ slider/src/js/main.js
@@ -5,18 +5,27 @@ $(document).ready(function(){
 });
 
 $(function(){
-    function getbg() {
-        $('[data-slide-index = 0]').on('click', function () {
-            $('body').css('background', '#6358c0');
-        });
-        $('[data-slide-index = 1]').on('click', function () {
-            $('body').css('background', '#61acec');
-        });
-        $('[data-slide-index = 2]').on('click', function () {
-            $('body').css('background', '#68a780');
-        });
-    }
-    getbg();
+    //function getbg() {
+    //    $('[data-slide-index = 0]').on('click', function () {
+    //        $('body').css('background', '#6358c0');
+    //    });
+    //    $('[data-slide-index = 1]').on('click', function () {
+    //        $('body').css('background', '#61acec');
+    //    });
+    //    $('[data-slide-index = 2]').on('click', function () {
+    //        $('body').css('background', '#68a780');
+    //    });
+    //}
+    //getbg();
+    var a1 = $('[data-slide-index = 0]');
+    var a2 = $('[data-slide-index = 1]');
+    var a3 = $('[data-slide-index = 2]');
+    console.log('a1', a1);
+    console.log('a2', a2);
+    console.log('a3', a3);
+
+
+
 
     function getmenu () {
         $('#openmenu').on('click', function(e) {

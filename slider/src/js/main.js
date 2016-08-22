88b488b02c850ecb0373bc024f183e97f41a8e4a
Kosarev Anton <rasto4itel@gmail.com>
2016-08-22 20:53:03 +0300

    jast commit

--- slider/src/js/main.js
+++ slider/src/js/main.js
@@ -4,62 +4,31 @@ $(document).ready(function(){
     $('.bxslider').bxSlider();
 });
 
-    //function getbg() {
-    //    $('[data-slide-index = 0]').on('click', function () {
-    //        $('body').css('background', '#6358c0');
-    //    });
-    //    $('[data-slide-index = 1]').on('click', function () {
-    //        $('body').css('background', '#61acec');
-    //    });
-    //    $('[data-slide-index = 2]').on('click', function () {
-    //        $('body').css('background', '#68a780');
-    //    });
-    //}
-    //getbg();
-    //var a = $('.bx-pager-link');
-
-$(document).ready(function() {
-    //var $a = $('div.bx-pager-item');
-    //var $a1 = $('[data-slide-index = 0]');
-    //var $a2 = $('[data-slide-index = 1]');
-    //var $a3 = $('[data-slide-index = 2]');
-    //var $bg = $('body');
-    //console.log('bg: ', $bg);
-    //console.log('$a: ', $a);
-    //console.log('$a: ', $a);
-    //$a1.click(function(){
-    //    $bg.css('background', '#6358c0');
-    //    console.log('a1: ', $a1);
-    //});
-    //$a.click(function(){
-    //    $bg.css('background', '#61acec');
-    //    console.log('a2: ', $a2);
-    //});
-    //$a3.click(function(){
-    //    $bg.css('background', '#68a780');
-    //    console.log('a3: ', $a3);
-    //});
-
-    var color = {
-        s1: '#6358c0',
-        s2: '#61acec',
-        s3: '#68a780'
-    };
-});
-
-
-
-
+//$('#openmenu').click(function() {
+//    $('.drop_menu').animate({
+//        display: 'block'
+//        //right: '0'
+//    }, 1000);
+//});
 
 $(function(){
     function getmenu () {
         $('#openmenu').on('click', function(e) {
             e.preventDefault();
-            $('.drop_menu').css('display', 'block');
+            $('.drop_menu').animate({
+                width: '540px'
+                //right: '0'
+            }, 1000);
+            //css('display', 'block');
         });
         $('#closemenu').on('click', function(e) {
             e.preventDefault();
-            $('.drop_menu').css('display', 'none');
+            $('.drop_menu').animate({
+                width: 0
+                //right: '0'
+            }, 1000);
+            //css('display', 'block');
+            //$('.drop_menu').css('display', 'none');
         });
     }
     getmenu();

c51711239408d7ae58cb49a8fca1ccc5a855f398
Kosarev Anton <rasto4itel@gmail.com>
2016-08-22 23:24:03 +0300

    jast commit

--- slider/src/js/main.js
+++ slider/src/js/main.js
@@ -5,6 +5,21 @@ $(document).ready(function(){
 });
 
 $(function(){
+    function openSub(click) {
+        $('.submenu').animate({
+            height: '140px'
+        }, 1000);
+        $(click).attr("class", "up");
+        click.innerHTML = "<img src='img/minus.png' alt='-'>";
+    }
+    function closeSub(click){
+        $('.submenu').animate({
+            height: '0'
+        }, 1000);
+        $(click).attr("class", "drop");
+        click.innerHTML = "<img src='img/plus.png' alt='+'>"
+        console.log(click.innerHTML);
+    }
 
     $('#openmenu').on('click', function(e) {
         e.preventDefault();
@@ -18,23 +33,16 @@ $(function(){
         $('.drop_menu').animate({
             width: 0
         }, 1000);
+        closeSub($('#drop'));
     });
 
     $('#drop').on('click', function (e) {
         e.preventDefault();
         var $class = $(this).attr("class");
         if ($class == 'drop') {
-            $('.submenu').animate({
-                height: '140px'
-            }, 1000);
-            $(this).attr("class", "up");
-            this.innerHTML = "<img src='img/minus.png' alt='-'>";
+            openSub(this);
         }else if ($class == 'up') {
-            $('.submenu').animate({
-                height: '0'
-            }, 1000);
-            $(this).attr("class", "drop");
-            this.innerHTML = "<img src='img/plus.png' alt='-'>";
+            closeSub(this);
         }
     });
 });

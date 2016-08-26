328133d3d6c7c0dddcdc70666f52d44a44588e0f
Kosarev Anton <rasto4itel@gmail.com>
2016-08-26 17:59:28 +0300

    jast commit

---
+++ frontend/src/js/main.js
@@ -0,0 +1,46 @@
+//= partials/jquery.bxslider.js
+var k=0;
+$(document).ready(function(){
+    $('.bxslider').bxSlider();
+});
+
+$(function(){
+    function openSub(click) {
+        $('.submenu').animate({
+            height: '140px'
+        }, 1000);
+        $(click).attr("class", "up");
+        click.innerHTML = "<img src='img/minus.png' alt='-'>";
+    }
+    function closeSub(click){
+        $('.submenu').animate({
+            height: '0'
+        }, 1000);
+        $(click).attr("class", "drop");
+        click.innerHTML = "<img src='img/plus.png' alt='+'>"
+        console.log(click.innerHTML);
+    }
+
+    $('#openmenu').on('click', function(e) {
+        e.preventDefault();
+        //$('.drop_menu').slideToggle();
+        $('.drop_menu').animate({width:'toggle'}, 500)
+    });
+
+    $('#closemenu').on('click', function(e) {
+        e.preventDefault();
+        //$('.drop_menu').slideToggle();
+        $('.drop_menu').animate({width:'toggle'}, 500)
+        closeSub($('#drop'));
+    });
+
+    $('#drop').on('click', function (e) {
+        e.preventDefault();
+        var $class = $(this).attr("class");
+        if ($class == 'drop') {
+            openSub(this);
+        }else if ($class == 'up') {
+            closeSub(this);
+        }
+    });
+});

11689ceba156cef20e7561701e2c7012d261c479
Kosarev Anton <rasto4itel@gmail.com>
2016-09-12 16:40:07 +0300

    fullcalendar plugin create

--- calendar/js/script.js
+++ calendar/js/script.js
@@ -3,11 +3,19 @@ var jsonTasks = {
 { "name": "Задача 2", "date": "2016-01-30", "time": "15:00:00", "category": "cat1", "VIP": true},
 { "name": "Задача 1", "date": "2016-01-30", "time": "4:00:00", "category": "cat2", "VIP": true}
 ]};
-
 var tasksStr = JSON.stringify(jsonTasks);
-
 console.log(tasksStr);
-
 var tasksObj = JSON.parse(tasksStr);
-
 console.log(tasksObj);
+console.log(tasksObj.tasks[0].name);
+
+
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+    })
+
+});

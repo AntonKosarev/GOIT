f0395bfc02a55e87079964041c10b60c416a6828
Kosarev Anton <rasto4itel@gmail.com>
2016-10-30 16:34:59 +0200

    end

--- DemingSofiya/js/script.js
+++ DemingSofiya/js/script.js
@@ -1,3 +1,11 @@
+/*Слайдер*/
 $(document).ready(function(){
     $('.bxslider').bxSlider();
 });
+
+/* Стилизация select*/
+(function($) {
+    $(function() {
+        $('.styler').styler();
+    });
+})(jQuery);
